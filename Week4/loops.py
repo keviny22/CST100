4d3bf710d083172507ac2b2897f8e641292c2022
Kevin Young <kevin_young@intuit.com>
2014-02-07 22:44:19 -0800

    week4

--- Week4/loops.py
+++ Week4/loops.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+#
+# Student:    Kevin S. Young
+# Assignment: Homework Week 4
+# Instructor: Robert Rucker
+# Due Date:   Feb 9, 2014
 
-# Homework Week4
 
 # Part I
+###########################
 # 1)
-
 for j in range(10):
     for i in range(10):
         print(i, end = " ")
@@ -18,18 +22,19 @@ for j in range(10):
     print()
 print()
 
+###########################
 # 2)
-
 for j in range(1,11):
     for i in range(j):
         print(i, end = " ")
     print()
 print()
 
+###########################
 # 3)
 for j in reversed(range(1,11)):
-    for k in range(j):
-        print(" ", end = '')
+    for k in range(abs(j-10)):
+        print('  ', end = '')
         
     for i in range(j):
         print(i, end = " ")
@@ -37,7 +42,8 @@ for j in reversed(range(1,11)):
 
 print()
 
-# 4)
+###########################
+## 4)
 n = 9
 for j in range(1,10):
     for i in range(j):
@@ -45,6 +51,3 @@ for j in range(1,10):
         print(n, end = " ")
     print()
 print()
-
-
-# Part II
---
+++ Week4/message.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#
+# Student:    Kevin S. Young
+# Assignment: Homework Week 4
+# Instructor: Robert Rucker
+# Due Date:   Feb 9, 2014
+
+# Part II - Decoding a secret message
+
+def encrypt(word,key):
+    mylist = []
+    for i in (list(word)):
+        mylist.append(encrypt_char(i,key))
+        
+    return mylist
+
+def encrypt_char(character,key):
+    character = ord(character)
+    if (character + key > 126):
+        enc_char = ((character + key) - 127) + 32
+    else:
+        enc_char = (character + key)
+        
+    return chr(enc_char)
+
+def decrypt(word,key):
+    mylist = []
+    for i in (list(word)):
+        mylist.append(decrypt_char(i,key))
+        
+    return mylist
+
+def decrypt_char(character,key):
+    character = ord(character)
+    if (character - key < 32):
+        dec_char = ((character - key) + 127) - 32
+    else:
+        dec_char = (character - key)
+        
+    return chr(dec_char)
+    
+    
+enc_input = input("Enter a regular message to encode:\n")
+enc_key   = int(input("Enter a key value (between 0 and 100) for encoding:\n"))
+
+for i in  encrypt(enc_input,enc_key):
+    print(i, end = '')
+print()
+
+dec_input = input("Enter an encrypted message to decode:\n")
+
+for j in range(100):
+    print('Key: ' + str(j) + ' -> Decoded Message: ', end='')
+    for i in decrypt(dec_input,j):
+        print(i, end = '')
+    print()
+
